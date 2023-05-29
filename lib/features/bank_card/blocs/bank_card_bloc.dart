b012fffc714cf634a4223cb91347fa2ec491f7ea
trieucq <cantrieu.2000@gmail.com>
2023-05-29 22:46:55 +0700

    Revert "them icon check, sort list tk ngan hang"
    This reverts commit ef0f6eff8fd63892a33f2cf789a73a83d9be6cf1.

--- lib/features/bank_card/blocs/bank_card_bloc.dart
+++ lib/features/bank_card/blocs/bank_card_bloc.dart
@@ -121,19 +121,12 @@ void _getBankAccounts(BankCardEvent event, Emitter emit) async {
   try {
     if (event is BankCardEventGetList) {
       emit(BankCardLoadingListState());
-      List<BankAccountDTO> list =
+      final List<BankAccountDTO> list =
           await bankCardRepository.getListBankAccount(event.userId);
       final List<Color> colors = [];
       PaletteGenerator? paletteGenerator;
       BuildContext context = NavigationService.navigatorKey.currentContext!;
       if (list.isNotEmpty) {
-        List<BankAccountDTO> listLinked =
-            list.where((e) => e.isAuthenticated).toList();
-        List<BankAccountDTO> listNotLinked =
-            list.where((e) => !e.isAuthenticated).toList();
-
-        list = [...listLinked, ...listNotLinked];
-
         for (BankAccountDTO dto in list) {
           NetworkImage image = ImageUtils.instance.getImageNetWork(dto.imgId);
           paletteGenerator = await PaletteGenerator.fromImageProvider(image);
