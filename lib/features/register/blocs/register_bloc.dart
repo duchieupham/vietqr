92a277dd6405ebe42db056adb7c096619ecbb180
trieucq <cantrieu.2000@gmail.com>
2023-06-08 22:13:12 +0700

    .

--- lib/features/register/blocs/register_bloc.dart
+++ lib/features/register/blocs/register_bloc.dart
@@ -10,27 +10,27 @@ class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
   RegisterBloc() : super(RegisterInitialState()) {
     on<RegisterEventSubmit>(_register);
   }
+
+  void _register(RegisterEvent event, Emitter emit) async {
+    try {
+      if (event is RegisterEventSubmit) {
+        emit(RegisterLoadingState());
+        ResponseMessageDTO responseMessageDTO =
+            await registerRepository.register(event.dto);
+        if (responseMessageDTO.status == Stringify.RESPONSE_STATUS_SUCCESS) {
+          emit(RegisterSuccessState());
+        } else {
+          String msg =
+              ErrorUtils.instance.getErrorMessage(responseMessageDTO.message);
+          emit(RegisterFailedState(msg: msg));
+        }
+      }
+    } catch (e) {
+      print('Error at register - RegisterBloc: $e');
+      emit(const RegisterFailedState(
+          msg: 'Không thể đăng ký. Vui lòng kiểm tra lại kết nối.'));
+    }
+  }
 }
 
 const RegisterRepository registerRepository = RegisterRepository();
-
-void _register(RegisterEvent event, Emitter emit) async {
-  try {
-    if (event is RegisterEventSubmit) {
-      emit(RegisterLoadingState());
-      ResponseMessageDTO responseMessageDTO =
-          await registerRepository.register(event.dto);
-      if (responseMessageDTO.status == Stringify.RESPONSE_STATUS_SUCCESS) {
-        emit(RegisterSuccessState());
-      } else {
-        String msg =
-            ErrorUtils.instance.getErrorMessage(responseMessageDTO.message);
-        emit(RegisterFailedState(msg: msg));
-      }
-    }
-  } catch (e) {
-    print('Error at register - RegisterBloc: $e');
-    emit(const RegisterFailedState(
-        msg: 'Không thể đăng ký. Vui lòng kiểm tra lại kết nối.'));
-  }
-}
