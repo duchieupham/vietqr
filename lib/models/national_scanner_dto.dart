56fe716f8bb374713e462472a2e7a59ac546c3f6
trieucq <trieucq@ati.vn>
2023-06-28 18:17:19 +0700

    qr

--- lib/models/national_scanner_dto.dart
+++ lib/models/national_scanner_dto.dart
@@ -16,4 +16,31 @@ class NationalScannerDTO {
     required this.address,
     required this.dateValid,
   });
+
+  String convertDate(String data) {
+    if (data.isNotEmpty) {
+      String day = data.substring(0, 2);
+      String month = data.substring(2, 4);
+      String year = data.substring(4, data.length);
+
+      return '$day/$month/$year';
+    }
+    return '';
+  }
+
+  String get getBirth => convertDate(birthdate ?? '');
+
+  String get getDateValid => convertDate(dateValid ?? '');
+
+  factory NationalScannerDTO.fromJson(list) {
+    return NationalScannerDTO(
+      nationalId: list[0],
+      oldNationalId: list[1],
+      fullname: list[2],
+      birthdate: list[3],
+      gender: list[4],
+      address: list[5],
+      dateValid: list[6],
+    );
+  }
 }
