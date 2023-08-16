94ef29d297d36e27ff20883b7c93d761fd894560
trieucq <trieucq@ati.vn>
2023-08-16 18:56:35 +0700

    luu account

--- lib/models/info_user_dto.dart
+++ lib/models/info_user_dto.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class InfoUserDTO {
   final String? phoneNo;
   final String? firstName;
@@ -5,8 +7,11 @@ class InfoUserDTO {
   final String? lastName;
   final String? imgId;
   final String? id;
+  final String? createdTime;
 
   get fullName => '${lastName ?? ''} ${middleName ?? ''} ${firstName ?? ''}';
+
+  DateTime get expiryAsDateTime => DateTime.parse(createdTime ?? '');
 
   factory InfoUserDTO.fromJson(Map<String, dynamic> json) {
     return InfoUserDTO(
@@ -16,7 +21,20 @@ class InfoUserDTO {
       lastName: json['lastName'] ?? '',
       imgId: json['imgId'] ?? '',
       id: json['id'] ?? '',
+      createdTime: DateTime.now().toIso8601String(),
     );
+  }
+
+  Map<String, dynamic> toSPJson() {
+    final Map<String, dynamic> data = {};
+    data['"id"'] = (id == '') ? '""' : '"$id"';
+    data['"firstName"'] = (firstName == '') ? '""' : '"$firstName"';
+    data['"middleName"'] = (middleName == '') ? '""' : '"$middleName"';
+    data['"lastName"'] = (lastName == '') ? '""' : '"$lastName"';
+    data['"phoneNo"'] = (phoneNo == '') ? '""' : '"$phoneNo"';
+    data['"imgId"'] = (imgId == '') ? '""' : '"$imgId"';
+    data['"createdTime"'] = (createdTime == '') ? '""' : '"$createdTime"';
+    return data;
   }
 
   InfoUserDTO(
@@ -25,5 +43,23 @@ class InfoUserDTO {
       this.middleName,
       this.lastName,
       this.imgId,
+      this.createdTime,
       this.id});
+}
+
+class ListLoginAccountDTO {
+  final List<InfoUserDTO> list;
+
+  ListLoginAccountDTO({required this.list});
+
+  factory ListLoginAccountDTO.fromJson(List? datas) {
+    List<InfoUserDTO> list = [];
+    if (datas == null || datas.isEmpty) {
+      return ListLoginAccountDTO(list: list);
+    }
+    try {
+      list = datas.map((f) => InfoUserDTO.fromJson(json.decode(f))).toList();
+    } catch (e, stack) {}
+    return ListLoginAccountDTO(list: list);
+  }
 }
