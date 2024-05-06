aec65cfd4bf39d15f85f2be5c0ab58e3295597d8
Kien.nh <kien.nh@vietqr.vn>
2024-05-06 12:10:23 +0700

    ggchat appbar

--- lib/features/connect_gg_chat/connect_gg_chat_screen.dart
+++ lib/features/connect_gg_chat/connect_gg_chat_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:vierqr/features/connect_gg_chat/states/connect_gg_chat_states.dart';
 
+import '../../commons/constants/configurations/app_images.dart';
 import '../../services/providers/connect_gg_chat_provider.dart';
 import 'blocs/connect_gg_chat_bloc.dart';
 
@@ -35,7 +36,55 @@ class __ScreenState extends State<_Screen> {
       builder: (context, state) {
         return Consumer<ConnectGgChatProvider>(
           builder: (context, value, child) {
-            return Scaffold();
+            return Scaffold(
+              backgroundColor: Colors.white,
+              resizeToAvoidBottomInset: true,
+              body: CustomScrollView(
+                physics: NeverScrollableScrollPhysics(),
+                slivers: [
+                  SliverAppBar(
+                    pinned: false,
+                    leadingWidth: 100,
+                    leading: InkWell(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.keyboard_arrow_left,
+                              color: Colors.black,
+                              size: 25,
+                            ),
+                            const SizedBox(width: 2),
+                            Text(
+                              "Trở về",
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 14),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    actions: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8),
+                        child: Image.asset(
+                          AppImages.icLogoVietQr,
+                          width: 95,
+                          fit: BoxFit.fitWidth,
+                        ),
+                      )
+                    ],
+                  ),
+                  SliverList(
+                    delegate: SliverChildListDelegate(<Widget>[]),
+                  )
+                ],
+              ),
+            );
           },
         );
       },
