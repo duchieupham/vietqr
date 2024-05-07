b277224a76d88ddf37bd255215ca79ad2c7acc62
nguyenlevietqr <nguyen.ldl@vietqr.vn>
2024-05-07 10:20:31 +0700

    update screen cnt ggchat

--- lib/features/connect_gg_chat/connect_gg_chat_screen.dart
+++ lib/features/connect_gg_chat/connect_gg_chat_screen.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:vierqr/commons/constants/configurations/theme.dart';
 import 'package:vierqr/commons/widgets/separator_widget.dart';
 import 'package:vierqr/features/connect_gg_chat/states/connect_gg_chat_states.dart';
 import 'package:vierqr/features/connect_gg_chat/views/info_gg_chat_widget.dart';
+import 'package:vierqr/models/trans/trans_request_dto.dart';
 
 import '../../commons/constants/configurations/app_images.dart';
 import '../../commons/enums/enum_type.dart';
 import '../../commons/utils/custom_button_switch.dart';
-import '../../commons/utils/image_utils.dart';
 import '../../commons/widgets/dialog_widget.dart';
 import '../../layouts/m_button_widget.dart';
 import '../../services/providers/connect_gg_chat_provider.dart';
@@ -42,6 +39,9 @@ class __ScreenState extends State<_Screen> {
   late ConnectGgChatProvider _provider;
   bool hasInfo = false;
   PageController _pageController = PageController(initialPage: 0);
+  int currentPageIndex = 0;
+  bool isEnableButton1 = true;
+  bool isEnableButton2 = true;
 
   @override
   void initState() {
@@ -83,7 +83,14 @@ class __ScreenState extends State<_Screen> {
                     leadingWidth: 100,
                     leading: InkWell(
                       onTap: () {
-                        Navigator.of(context).pop();
+                        if (currentPageIndex > 0) {
+                          _pageController.previousPage(
+                            duration: Duration(milliseconds: 300),
+                            curve: Curves.easeInOut,
+                          );
+                        } else {
+                          Navigator.of(context).pop();
+                        }
                       },
                       child: Container(
                         padding: const EdgeInsets.only(left: 8),
@@ -123,10 +130,17 @@ class __ScreenState extends State<_Screen> {
                               height: MediaQuery.of(context).size.height,
                               child: PageView(
                                 controller: _pageController,
+                                physics: NeverScrollableScrollPhysics(),
+                                onPageChanged: (int index) {
+                                  setState(() {
+                                    currentPageIndex = index;
+                                  });
+                                },
                                 children: [
                                   startConnectGgChat(),
                                   listAccountLinked(),
                                   start1ConnectGgChat(),
+                                  finishConnectGgChat(),
                                 ],
                               ),
                             )
@@ -178,8 +192,43 @@ class __ScreenState extends State<_Screen> {
     );
   }
 
+  Widget finishConnectGgChat() {
+    return Container(
+      padding: EdgeInsets.only(left: 40),
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 20),
+          Container(
+            height: 100,
+            width: 100,
+            child: Image.asset('assets/images/ic-gg-chat-home.png'),
+          ),
+          SizedBox(height: 30),
+          Container(
+            width: 250,
+            height: 60,
+            child: Text(
+              'Kết nối Google Chat\nthành công!',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            width: double.infinity,
+            child: Text(
+              'Cảm ơn quý khách đã sử dụng dịch vụ \nVietQR VN của chúng tôi.',
+              style: TextStyle(fontSize: 15),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget start1ConnectGgChat() {
-    final height = MediaQuery.of(context).size.height;
+    bool isValid = false;
     return Container(
       padding: EdgeInsets.only(left: 40),
       width: double.infinity,
@@ -200,12 +249,85 @@ class __ScreenState extends State<_Screen> {
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
             ),
           ),
-          SizedBox(height: 20),
+          SizedBox(height: 30),
           Container(
-            width: double.infinity,
+            height: 50,
+            margin: EdgeInsets.only(right: 40),
+            decoration: BoxDecoration(
+              color: AppColor.BLUE_TEXT.withOpacity(0.3),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 15,
+                  height: 15,
+                  child: Image.asset('assets/images/ic-guides-ggchat.png'),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Xem hướng dẫn tại đây',
+                  style: TextStyle(color: AppColor.BLUE_TEXT, fontSize: 15),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 30),
+          Container(
+            height: 20,
             child: Text(
-              'Thực hiện kết nối chỉ với một vài thao tác đơn giản.\nAn tâm về vấn đề an toàn thông tin - dữ liệu\ncủa Google Chat mang lại.',
+              'Webhook Google Chat',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 40),
+            height: 50,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(
+                  bottom: BorderSide(color: AppColor.GREY_TEXT, width: 0.5),
+                )),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Nhập đường dẫn Webhook tại đây',
+                  hintStyle: TextStyle(fontSize: 15, color: AppColor.GREY_TEXT),
+                  contentPadding: EdgeInsets.symmetric(vertical: 15)),
               style: TextStyle(fontSize: 15),
+            ),
+          ),
+          Container(
+            height: 20,
+            margin: EdgeInsets.only(right: 40),
+            alignment: Alignment.centerRight,
+            child: Visibility(
+              visible: !isValid,
+              child: Text(
+                'Webhook không hợp lệ.',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 20,
+            margin: EdgeInsets.only(right: 40),
+            alignment: Alignment.centerRight,
+            child: Visibility(
+              visible: !isValid,
+              child: Text(
+                'Vui lòng kiểm tra lại thông tin.',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 15,
+                ),
+              ),
             ),
           ),
         ],
@@ -214,7 +336,6 @@ class __ScreenState extends State<_Screen> {
   }
 
   Widget listAccountLinked() {
-    final height = MediaQuery.of(context).size.height;
     return Consumer<ConnectGgChatProvider>(
       builder: (context, provider, child) {
         return Container(
@@ -321,36 +442,53 @@ class __ScreenState extends State<_Screen> {
   }
 
   Widget bottomButton() {
+    bool isEnable = true;
+    String buttonText = '';
+    if (currentPageIndex == 0) {
+      buttonText = 'Bắt đầu kết nối';
+    } else if (currentPageIndex == 1) {
+      buttonText = 'Tiếp tục';
+      isEnable = isEnableButton1;
+    } else if (currentPageIndex == 2) {
+      buttonText = 'Kết nối';
+      isEnable = isEnableButton2;
+    } else if (currentPageIndex == 3) {
+      buttonText = 'Hoàn thành';
+    }
+
+    Color textColor = isEnable ? AppColor.WHITE : AppColor.BLACK;
+    Color iconColor = isEnable ? AppColor.WHITE : AppColor.BLACK;
+    Color icon1Color = isEnable ? AppColor.BLUE_TEXT : AppColor.GREY_BUTTON;
+
     return Container(
       width: double.infinity,
       child: MButtonWidget(
         margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
         height: 50,
-        isEnable: true,
+        isEnable: isEnable,
+        colorDisableBgr: AppColor.GREY_BUTTON,
+        colorDisableText: AppColor.BLACK,
         title: '',
         onTap: () {
-          _pageController.nextPage(
-              duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
+          if (currentPageIndex < 2) {
+            _pageController.nextPage(
+                duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
+          } else if (currentPageIndex == 2) {
+            _pageController.nextPage(
+                duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
+          } else {
+            Navigator.of(context).pop();
+            // Thực hiện kết nối khi ở trang cuối cùng
+          }
         },
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Icon(
-              Icons.arrow_forward,
-              color: AppColor.BLUE_TEXT,
-              size: 20,
-            ),
-            Text(
-              'Bắt đầu kết nối',
-              style: TextStyle(fontSize: 15, color: AppColor.WHITE),
-            ),
+            Icon(Icons.arrow_forward, color: icon1Color, size: 20),
+            Text(buttonText, style: TextStyle(fontSize: 15, color: textColor)),
             Padding(
               padding: const EdgeInsets.only(right: 10),
-              child: Icon(
-                Icons.arrow_forward,
-                color: AppColor.WHITE,
-                size: 20,
-              ),
+              child: Icon(Icons.arrow_forward, color: iconColor, size: 20),
             ),
           ],
         ),
