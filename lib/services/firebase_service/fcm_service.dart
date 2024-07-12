56b3d758713b2e4ad3edbd0d9870e4a45d4a015b
nguyenlevietqr <nguyen.ldl@vietqr.vn>
2024-07-12 15:25:32 +0700

    add logic notification sound

--- lib/services/firebase_service/fcm_service.dart
+++ lib/services/firebase_service/fcm_service.dart
@@ -20,11 +20,12 @@ class FCMService {
       LOG.info(
           "Push notification received: ${message.notification?.title} - ${message.notification?.body}");
       LOG.info("receive data: ${message.data}");
-      NotificationService().showNotification(
-        title: message.notification?.title,
-        body: message.notification?.body,
-        payload: json.encode(message.data),
-      );
+      if (message.data['notificationType'] != 'N05')
+        NotificationService().showNotification(
+          title: message.notification?.title,
+          body: message.notification?.body,
+          payload: json.encode(message.data),
+        );
 
       //process when receive data
       if (message.data.isNotEmpty) {
