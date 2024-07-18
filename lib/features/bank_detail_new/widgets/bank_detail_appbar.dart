3d84d32cf43afd48bdbe2b3206eb0e65f3770f15
chuthuong13492 <kien.nh@vietqr.vn>
2024-07-18 17:41:50 +0700

    check scroll appbar

--- lib/features/bank_detail_new/widgets/bank_detail_appbar.dart
+++ lib/features/bank_detail_new/widgets/bank_detail_appbar.dart
@@ -5,9 +5,13 @@ import 'package:vierqr/commons/constants/configurations/theme.dart';
 class BankDetailAppbar extends StatefulWidget {
   final Function(int) onSelect;
   final int selected;
+  final ValueNotifier<bool> isScroll;
 
   const BankDetailAppbar(
-      {super.key, required this.onSelect, required this.selected});
+      {super.key,
+      required this.onSelect,
+      required this.isScroll,
+      required this.selected});
 
   @override
   State<BankDetailAppbar> createState() => _BankDetailAppbarState();
@@ -22,86 +26,94 @@ class _BankDetailAppbarState extends State<BankDetailAppbar> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      padding: const EdgeInsets.only(top: 50, bottom: 0),
-      // color: AppColor.TRANSPARENT,
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color(0xFFE1EFFF),
-            Color(0xFFE5F9FF),
-          ],
-          end: Alignment.centerRight,
-          begin: Alignment.centerLeft,
-        ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const SizedBox(
-            width: 20,
+    return ValueListenableBuilder<bool>(
+      valueListenable: widget.isScroll,
+      builder: (context, value, child) {
+        return Container(
+          height: 100,
+          padding: const EdgeInsets.only(top: 50, bottom: 0),
+          // color: AppColor.TRANSPARENT,
+          decoration: BoxDecoration(
+            color: value ? AppColor.WHITE : AppColor.WHITE,
+            gradient: value
+                ? const LinearGradient(
+                    colors: [
+                      Color(0xFFE1EFFF),
+                      Color(0xFFE5F9FF),
+                    ],
+                    end: Alignment.centerRight,
+                    begin: Alignment.centerLeft,
+                  )
+                : null,
           ),
-          GestureDetector(
-            onTap: () {
-              Navigator.of(context).pop();
-            },
-            child: const Icon(
-              Icons.arrow_back_ios,
-              size: 20,
-            ),
-          ),
-          // Spacer(),
-          const SizedBox(
-            width: 10,
-          ),
-          ...listTitle.map(
-            (title) {
-              int index = listTitle.indexOf(title);
-              bool isSelected = widget.selected == index;
-              return GestureDetector(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                width: 20,
+              ),
+              GestureDetector(
                 onTap: () {
-                  widget.onSelect(index);
-                  // setState(() {
-                  //   _selectedIndex = index;
-                  // });
-                  // print(title);
+                  Navigator.of(context).pop();
                 },
-                child: Container(
-                  height: 30,
-                  width: 80,
-                  margin: const EdgeInsets.symmetric(horizontal: 8),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 6, horizontal: 4),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    gradient: isSelected
-                        ? const LinearGradient(
-                            colors: [
-                              Color(0xFF00C6FF),
-                              Color(0xFF0072FF),
-                            ],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                          )
-                        : null,
-                  ),
-                  child: Text(
-                    title,
-                    maxLines: 1,
-                    style: TextStyle(
-                      color: isSelected ? Colors.white : AppColor.GREY_TEXT,
-                      fontSize: 14,
-                    ),
-                  ),
+                child: const Icon(
+                  Icons.arrow_back_ios,
+                  size: 20,
                 ),
-              );
-            },
+              ),
+              // Spacer(),
+              const SizedBox(
+                width: 10,
+              ),
+              ...listTitle.map(
+                (title) {
+                  int index = listTitle.indexOf(title);
+                  bool isSelected = widget.selected == index;
+                  return GestureDetector(
+                    onTap: () {
+                      widget.onSelect(index);
+                      // setState(() {
+                      //   _selectedIndex = index;
+                      // });
+                      // print(title);
+                    },
+                    child: Container(
+                      height: 30,
+                      width: 80,
+                      margin: const EdgeInsets.symmetric(horizontal: 8),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 6, horizontal: 4),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: isSelected
+                            ? const LinearGradient(
+                                colors: [
+                                  Color(0xFF00C6FF),
+                                  Color(0xFF0072FF),
+                                ],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              )
+                            : null,
+                      ),
+                      child: Text(
+                        title,
+                        maxLines: 1,
+                        style: TextStyle(
+                          color: isSelected ? Colors.white : AppColor.GREY_TEXT,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const Spacer(),
+            ],
           ),
-          const Spacer(),
-        ],
-      ),
+        );
+      },
     );
   }
 }
