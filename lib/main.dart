c051033b9ee2a3f2c1df65caf722bd0b78a05594
chuthuong13492 <kien.nh@vietqr.vn>
2024-07-26 22:49:08 +0700

    change version

--- lib/main.dart
+++ lib/main.dart
@@ -91,7 +91,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // await Injection.inject(env: EnvType.PROD);
-  await Injection.inject(env: EnvType.STG);
+  await Injection.inject(env: EnvType.PROD);
 
   await SharePrefUtils.init();
   await SharePrefUtils.onClearCache();
